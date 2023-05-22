507dbd00e4c560fd70631c28931d327857d732fc
Fabiola Magalhaes <fabimagal99@gmail.com>
2023-05-22 10:33:48 -0300

    Desligar um colaborador.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,79 @@
+import Models.Cargo;
+import Models.Colaborador;
+import Models.Nivel;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Date;
+
+import static Models.Colaborador.*;
+
 public class Main {
+
+    static ArrayList<Colaborador> colaboradores = new ArrayList<>();
+    static ArrayList<Cargo> cargos = new ArrayList<>();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Exercicio 03
+        cargos.add(new Cargo("Desenvolvedor Python", 5500.0, Nivel.JUNIOR));
+        cargos.add(new Cargo("Desenvolvedor Fullstack", 7500.0, Nivel.PLENO));
+        cargos.add(new Cargo("Desenvolvedor Front-End", 9500.0, Nivel.SENIOR));
+
+        colaboradores.add(new Colaborador("Mônica",new Date(),cargos.get(0)));
+        colaboradores.add(new Colaborador("Sérgio",new Date(),cargos.get(1)));
+        colaboradores.add(new Colaborador("Julia",new Date(),cargos.get(2)));
+
+        //colaboradorSemDesligamento(colaboradores);
+        //listarColaboradorPorCargo(cargos, colaboradores);
+
+        //Exercicio 7
+        //adicionarColaborador(novoColaborador());
+//        Colaborador colaborador = novoColaborador();
+//        if (colaborador != null){
+//            adicionarColaborador(colaborador);
+//        }
+        //Exercicio 8
+        desligarColaborador();
     }
+
+    public static Colaborador novoColaborador(){
+        String nome = JOptionPane.showInputDialog(null, "Digite o nome do novo colaborador:");
+        Date dtAdmissao = new Date();
+        Cargo cargo = (Cargo) JOptionPane.showInputDialog(
+                null,
+                "Selecione um cargo",
+                "Seleção do cargo",
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                cargos.toArray(),
+                cargos.get(0)
+        );
+        Colaborador colaborador = new Colaborador(nome, dtAdmissao, cargo);
+        System.out.println("Novo colaborador adicionado com sucesso! " + colaborador);
+        return colaborador;
+    }
+    public static void adicionarColaborador(Colaborador colaborador){
+        colaboradores.add(colaborador);
+    }
+    public static void desligarColaborador(){
+        JOptionPane.showMessageDialog(null, "Desligar próximo colaborador");
+        Colaborador colaborador = (Colaborador) JOptionPane.showInputDialog(
+                null,
+                "Selecione o colaborador",
+                "Seleção do Caloborador",
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                colaboradores.toArray(),
+                colaboradores.get(0)
+        );
+        colaborador.setDtDesligamento(new Date());
+        System.out.println("Colaborador descadastrado com sucesso em " + colaborador.getDtDesligamento());
+
+    }
+
+
+
+
+
 }
--- src/Models/Colaborador.java
+++ src/Models/Colaborador.java
@@ -1,5 +1,7 @@
 package Models;
 
+import javax.swing.*;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Colaborador {
@@ -8,5 +10,52 @@ public class Colaborador {
     private Date dtDesligamento;
     private Cargo cargo;
     private Double salario;
+
+    public Colaborador(String nome, Date dtAdmissao, Cargo cargo) {
+        this.nome = nome;
+        this.dtAdmissao = dtAdmissao;
+        this.cargo = cargo;
+        this.salario = cargo.getSalarioBase();
+    }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public Date getDtAdmissao() { return dtAdmissao; }
+    public void setDtAdmissao(Date dtAdmissao) { this.dtAdmissao = dtAdmissao; }
+    public Date getDtDesligamento() { return dtDesligamento; }
+    public void setDtDesligamento(Date dtDesligamento) { this.dtDesligamento = dtDesligamento; }
+    public Cargo getCargo() { return cargo; }
+    public void setCargo(Cargo cargo) { this.cargo = cargo; }
+    public Double getSalario() { return salario; }
+    public void setSalario(Double salario) { this.salario = salario; }
+    @Override
+    public String toString() {
+        return "Colaborador{" +
+                "nome='" + nome + '\'' +
+                ", dtAdmissao=" + dtAdmissao +
+                ", dtDesligamento=" + dtDesligamento +
+                ", cargo=" + cargo +
+                ", salario=" + salario +
+                '}';
+    }
+
+    //Faça um método para listar colaboradores da lista de colaboradores que não possuem data de desligamento.
+    public static void colaboradorSemDesligamento(ArrayList<Colaborador> colaboradores){
+        for (Colaborador colaborador : colaboradores){
+            if (colaborador.getDtDesligamento() == null){
+                System.out.println(colaborador);
+            }
+        }
+    }
+
+    public static void listarColaboradorPorCargo(ArrayList<Cargo> cargos, ArrayList<Colaborador> colaboradores){
+        for (Cargo cargo : cargos){
+            for (Colaborador colaborador : colaboradores){
+                if (colaborador.getCargo() == cargo){
+                    System.out.println(colaborador);
+                }
+            }
+        }
+    }
+
 
 }
